13821350cbb43d360dba6ec31bae95c569427c74
Gueta <ricardogueta@hotmail.com>
2023-07-19 11:54:31 -0700

    getting codes info, and formatting grid 1

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -21,26 +21,27 @@ export class DashboardComponent implements OnInit {
   }
 
   getCodes(){
-    let today = new Date().toISOString();
-    console.log('Now -->', today)
     this.http.get('http://192.168.1.185:5000/api/codes/').subscribe((data) =>{
      this.list = data;
       this.list.forEach((element:any) => {
-        console.log('expiry ->',element.expiry)
-        if(element.expiry > today){
-          console.log('Si expiro')
-          this.list[0].expiro = true;
-          
-        }else{
+        let comp = (this.compareDates(new Date(element.expiry),new Date()));
+        if(comp = 1){
           this.list[0].expiro = false;
+        }else{
+          this.list[0].expiro = true;
         }
       });
-
-      // this.list =  data;
     });
 
     console.table(this.list);
   }
+
+  compareDates = (a: Date, b: Date): number => {
+    if (a < b) return -1;
+    if (a > b) return +1;
+  
+    return 0; // dates are equal
+  };
 
   toggleMenu(){
     this.isMenuOpened = !this.isMenuOpened;
